1e0a659b221574a9eceab5fa0e41a7f4062122f2
Elyssia Widjaja <Elyssia@Elyssias-MBP.lan1>
2019-08-19 16:08:35 -0700

    reached MVP

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Controls = props => {
   const { locked, closed, toggleLocked, toggleClosed } = props;
 
   return (
-    <div className="controls panel">
+    <div data-testid="controls" className="controls panel">
       <button disabled={!closed} onClick={toggleLocked} className="toggle-btn">
-        {locked ? 'Unlock Gate' : 'Lock Gate'}
+        {locked ? "Unlock Gate" : "Lock Gate"}
       </button>
-      <button disabled={locked} onClick={toggleClosed} className="toggle-btn">
-        {closed ? 'Open Gate' : 'Close Gate'}
+      <button
+        data-testid="openClose"
+        disabled={locked}
+        onClick={toggleClosed}
+        className="toggle-btn"
+      >
+        {closed ? "Open Gate" : "Close Gate"}
       </button>
     </div>
   );
